8f8d02e844149526f337d6441844b253a96e5724
prathamesh sharad derore <77142866+pratha56@users.noreply.github.com>
2021-07-01 23:26:03 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,16 +2,18 @@
 """
 Created on Thu Jul  1 22:01:36 2021
 
-@author: asus
+@: asus
 """
 
 import streamlit as st
 import pandas as pd
 import numpy as np
-df=pd.read_csv(r'C:\Users\asus\Desktop\my desk\data science\hirearchical clustering\Universities.csv')
+import seaborn as sns
 st.title('My project')
 st.header('prathamesh deore')
+df=sns.load_dataset('iris')
 st.dataframe(df)
-st.bar_chart(df['SAT'])
+st.bar_chart(df['sepal_length'])
+
 
 
